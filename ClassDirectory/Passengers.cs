d430356f96b952a1132080216ef2e13b94dcb644
DorianLeci <dleci00@fesb.hr>
2025-11-16 16:59:16 +0100

    Add function "MainMenu".Add class Passenger

---
+++ ClassDirectory/Passengers.cs
@@ -0,0 +1,13 @@
+namespace Internship_3_OOP.ClassDirectory;
+
+public class Passengers
+{
+    public int Id { get; }
+    public string Name { get; }
+    public string Surname { get; }
+    public string Email { get; }
+    public string Phone { get; }
+    public string Password { get; }
+    public DateOnly BirthDate { get; }
+    
+}
--- Program.cs
+++ Program.cs
@@ -1,3 +1,40 @@
 ﻿// See https://aka.ms/new-console-template for more information
+namespace Internship_3_OOP;
 
-Console.WriteLine("Hello, World!");
+class Program
+{
+    static void Main(string[] args)
+    {}
+        static void MainMenu()
+    {
+        while (true)
+        {
+            Console.WriteLine("----------------------");
+            Console.WriteLine("1 - Putnici\n");
+            Console.WriteLine("2 - Letovi\n");
+            Console.WriteLine("3 - Avioni\n");
+            Console.WriteLine("4 - Posada\n");
+            Console.WriteLine("5 - Izlaz iz programa");
+            Console.WriteLine("----------------------\n");
+            if (int.TryParse(Console.ReadLine(), out int inputMainMenu))
+            {
+                switch (inputMainMenu)
+                {
+                    case 5:
+                        Console.WriteLine("Uspješan odabir.Izlaz iz aplikacije\n");
+                        Environment.Exit(0);
+                        break;
+                    case 1:
+                        Console.WriteLine("Uspješan odabir izbornika za putnike.\n");
+                        break;
+                    default:
+                        Console.WriteLine("Unos nije među ponuđenima.Unesi ponovno");
+                        break;
+                }
+            }
+            else
+            {
+                Console.WriteLine("\nPogrešan tip podatka->unesi cijeli broj.");
+            }
+        }
+}
